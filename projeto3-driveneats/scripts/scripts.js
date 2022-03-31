30fcf471a85385531c7276183a88f090cfbee49a
Nayara <nrdeandrade@gmail.com>
2022-03-31 20:11:30 -0300

    layout

---
+++ projeto3-driveneats/scripts/scripts.js
@@ -0,0 +1,61 @@
+let prato;
+let bebida;
+let sobremesa;
+let custoPrato;
+let custoBebida;
+let custoSobremesa;
+let valor;
+
+function escolherPrato1() {
+    document.getElementById("prato1").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("prato2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("prato3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang1";
+    custoPrato = 14.9;
+  
+}
+
+function escolherPrato2() {
+    document.getElementById("prato2").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("prato1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("prato3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang2";
+    custoPrato = 14.9;
+  
+}
+
+function escolherPrato3() {
+    document.getElementById("prato3").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("prato2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("prato1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang3";
+    custoPrato = 14.9;
+  
+}
+
+function escolherBebida1() {
+    document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang1";
+    custoPrato = 14.9;
+  
+}
+
+function escolherBebida2() {
+    document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang2";
+    custoPrato = 14.9;
+  
+}
+
+function escolherBebida3() {
+    document.getElementById("bebida3").style.boxShadow = "inset 0 0 0 5px #32B72F";
+    document.getElementById("bebida2").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    document.getElementById("bebida1").style.boxShadow = "inset 0 0 0 0px #32B72F";
+    prato = "Frango Yin Yang3";
+    custoPrato = 14.9;
+  
+}
